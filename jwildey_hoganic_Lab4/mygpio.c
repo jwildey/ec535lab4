96296853f0ea0c111f660d8008dcaac770fea299
Josh Wildey <jwildey@gmail.com>
2017-03-26 18:22:58 -0400

    added GPIO initialization and interrupts to handle the buttons

--- jwildey_hoganic_Lab4/mygpio.c
+++ jwildey_hoganic_Lab4/mygpio.c
@@ -27,14 +27,28 @@
 #include <linux/list.h>
 #include <asm/string.h>
 #include <linux/signal.h>
+#include <asm/arch/hardware.h> /* GPIO access */
+#include <asm/arch/gpio.h>
+#include <asm/arch/pxa-regs.h>
+#include <asm/arch/irqs.h>  /* interrupts */
+#include <linux/interrupt.h>
+
+#define GPIO_BTN0 17
+#define GPIO_BTN1 101
+#define GPIO_LED0 28
+#define GPIO_LED1 29
+#define GPIO_LED2 30
+#define GPIO_LED3 31
+
+#define GPIO_HIGH 1
+#define GPIO_LOW  0
 
 /************************************
  * Set Module Info
  ************************************/
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("GPIO Buttons/LEDs");
-MODULE_AUTHOR("Josh Wildey");
-MODULE_AUTHOR("Ian Hogan");
+MODULE_AUTHOR("Josh Wildey, Ian Hogan");
 
 /************************************
  * Declaration of mygpio.c functions
@@ -51,6 +65,8 @@ static ssize_t mygpio_write(struct file *filp,
                              loff_t *f_pos);
 static void mygpio_exit(void);
 static int mygpio_init(void);
+static irqreturn_t btn0Handler(unsigned int irq, void *dev_id, struct pt_regs *regs);
+static irqreturn_t btn1Handler(unsigned int irq, void *dev_id, struct pt_regs *regs);
 
 /************************************
  * Structure Definitions
@@ -78,6 +94,46 @@ module_init(mygpio_init);
 module_exit(mygpio_exit);
 
 /**
+ * TODO
+ */
+static irqreturn_t btn0Handler(unsigned int irq, void *dev_id, struct pt_regs *regs)
+{
+    printk(KERN_INFO "BTN0 Handler!!!!\n");
+    printk(KERN_INFO "GPIO_LED0: %d\n", pxa_gpio_get_value(GPIO_LED0));
+    /*if(gpio_get_value(GPIO_LED0) > 0)
+    {
+        printk(KERN_INFO "GPIO_LED0 is high, setting to low\n");
+        //gpio_set_value(GPIO_LED0, GPIO_LOW);
+    }
+    else
+    {
+        printk(KERN_INFO "GPIO_LED0 is low, setting to high\n");
+        //gpio_set_value(GPIO_LED0, GPIO_HIGH);
+    }*/
+    return IRQ_HANDLED;
+}
+
+/**
+ * TODO
+ */
+static irqreturn_t btn1Handler(unsigned int irq, void *dev_id, struct pt_regs *regs)
+{
+    printk(KERN_INFO "BTN1 Handler!!!!\n");
+    printk(KERN_INFO "GPIO_LED0: %d\n", pxa_gpio_get_value(GPIO_LED1));
+    /*if(gpio_get_value(GPIO_LED1) > 0)
+    {
+        printk(KERN_INFO "GPIO_LED1 is high, setting to low\n");
+        //gpio_set_value(GPIO_LED1, GPIO_LOW);
+    }
+    else
+    {
+        printk(KERN_INFO "GPIO_LED1 is low, setting to high\n");
+        //gpio_set_value(GPIO_LED1, GPIO_HIGH);
+    }*/
+    return IRQ_HANDLED;
+}
+
+/**
  *  \brief Initialize mygpio module
  *
  *  The kernel space function which corresponds to inserting
@@ -99,6 +155,35 @@ static int mygpio_init(void)
 		return result;
 	}
 
+    // Request GPIO pins
+    gpio_request(GPIO_BTN0, "BTN0");
+    gpio_request(GPIO_BTN1, "BTN1");
+    gpio_request(GPIO_LED0, "LED0");
+    gpio_request(GPIO_LED1, "LED1");
+    gpio_request(GPIO_LED2, "LED2");
+    gpio_request(GPIO_LED3, "LED3");
+
+    // Set GPIO pin direction
+    gpio_direction_input(GPIO_BTN0);
+    gpio_direction_input(GPIO_BTN1);
+    gpio_direction_output(GPIO_LED0, GPIO_DFLT_LOW);
+    gpio_direction_output(GPIO_LED1, 0);
+    gpio_direction_output(GPIO_LED2, 0);
+    gpio_direction_output(GPIO_LED3, 0);
+
+    // Setup IRQs
+    result = request_irq(gpio_to_irq(GPIO_BTN0),                     //requested interrupt
+                         (irq_handler_t) btn0Handler,                // pointer to handler function
+                         IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, // interrupt mode flag
+                         "btn0Handler",                              // used in /proc/interrupts
+                         NULL);                 // the *dev_id shared intterupt lines, NULL is okay
+
+    result = request_irq(gpio_to_irq(GPIO_BTN1),                     //requested interrupt
+                         (irq_handler_t) btn1Handler,                // pointer to handler function
+                         IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, // interrupt mode flag
+                         "btn1Handler",                              // used in /proc/interrupts
+                         NULL);                 // the *dev_id shared intterupt lines, NULL is okay
+
     printk(KERN_INFO "mygpio: module loaded.\n"); 
 
 	return 0;
@@ -115,6 +200,18 @@ static void mygpio_exit(void)
 {
 	// Freeing the major number
 	unregister_chrdev(mygpio_major, "mygpio");
+
+    // Free up IRQs
+    free_irq(gpio_to_irq(GPIO_BTN0), NULL);
+    free_irq(gpio_to_irq(GPIO_BTN1), NULL);
+
+    // Free up GPIOs
+    gpio_free(GPIO_BTN0);
+    gpio_free(GPIO_BTN1);
+    gpio_free(GPIO_LED0);
+    gpio_free(GPIO_LED1);
+    gpio_free(GPIO_LED2);
+    gpio_free(GPIO_LED3);
 
 	printk(KERN_INFO "mygpio: module unloaded.\n");
 }
